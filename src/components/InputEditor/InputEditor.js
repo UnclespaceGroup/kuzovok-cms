00d912eddf1a12c097152cc2b6bff1c5c00bfead
Ivan Dmitrachkov <dmitrachkovivan@gmail.com>
2020-01-08 14:33:18 +0300

    fix

--- src/components/InputEditor/InputEditor.js
+++ src/components/InputEditor/InputEditor.js
@@ -1,32 +1,65 @@
-import React, { useRef, useState, useMemo } from 'react'
+import React, { useState, useEffect } from 'react'
+import ReactSummernote from 'react-summernote';
+import 'react-summernote/dist/react-summernote.css';
+import 'react-summernote/lang/summernote-ru-RU';
+import 'bootstrap/js/src/modal';
+import 'bootstrap/js/src/dropdown';
+import 'bootstrap/js/src/tooltip';
+import Compressor from 'compressorjs';
 
-import 'jodit/build/jodit.min.css'
-import JoditEditor from 'jodit-react'
+const InputEditor = ({ input, ...props }) => {
+  const [_content, setContent] = useState()
 
-const InputEditor = ({ input }) => {
-  const [ val, setVal ] = useState()
-  const editor = useRef(null)
+  useEffect(() => {
+    let text = ''
+    let newText = props.text
+    if (newText.blocks) {
+      newText.blocks.map((res) => {
+        text += res.text
+      })
+    } else text = props.text
+    console.log(props)
+    setContent(text)
+  }, [])
 
-  useMemo(() => {
-    setVal(input.value)
-  }, [input.value])
+  const onImageUpload = (fileList) => {
+    new Compressor(fileList[0], {
+      maxWidth: 480,
+      success(result) {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          ReactSummernote.insertImage(reader.result);
+        }
 
-  // const config = {
-  //   uploader: {
-  //     insertImageAsBase64URI: true
-  //   },
-  //   minHeight: 500,
-  //   language: 'ru',
-  //   readonly: false,
-  //   toolbarSticky: false,
-  //   disablePlugins: 'hotkeys'
-  // }
+        reader.readAsDataURL(result);
 
+      }
+    })
+  }
+
+  const onChange = function (content) {
+    setContent(content)
+    props.onEditText(content, content)
+  }
   return (
-    <div onBlur={() => {input.onChange(editor.current.state.value)}}>
-      <JoditEditor
-        value={val}
-        ref={editor}
+    <div>
+      <ReactSummernote
+        value={_content}
+        options={{
+          lang: 'ru-RU',
+          height: 350,
+          toolbar: [
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['font', ['strikethrough', 'superscript', 'subscript']],
+            ['fontsize', ['fontsize']],
+            ['table', ['table']],
+            ['insert', ['link', 'picture', 'video']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']],
+          ]
+        }}
+        onImageUpload={onImageUpload}
+        onChange={onChange}
       />
     </div>
   )
