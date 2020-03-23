1b6b6ba897d50163b8c1d3ccdc549b8eb57f7abd
Ivan Dmitrachkov <dmitrachkovivan@gmail.com>
2020-03-24 00:32:16 +0300

    add: wysiwiq

--- src/components/Wysiwyg/useWysiwyg.js
+++ src/components/Wysiwyg/useWysiwyg.js
@@ -1,5 +1,5 @@
 import { BASE_URL } from 'constants/url'
-const pathTemplate = '__path__'
+const pathTemplate = '__SERVER_PATH__'
 
 const useWysiwyg = ({ children }) => {
   const text = typeof children === 'string' && children.replace(new RegExp(pathTemplate, 'g'), BASE_URL)
